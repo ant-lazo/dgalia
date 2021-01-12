7350fda6e704edbe223d64575bebf4f0b335ef52
jonathan padilla <jonathan962911@gmail.com>
2021-01-11 21:30:04 -0500

    Primer commit

---
+++ src/app/application/core/config/pages-.config.ts
@@ -0,0 +1,12 @@
+export const PagesConfig = {
+    sigIng: {
+        title: 'Iniciar sesión',
+        emailAddressInput: 'Correo electronico',
+        passwordInput: 'Contraseña',
+        rememberLabel: 'Recordar contraseña',
+        forgotPasswordLabel: 'Recordar contraseña',
+        dignInButtonText: 'Inciar Sesión',
+        backgroudTextTitle: 'EL PRIMER CAMPUS CULINARIO DEL PERÚ',
+        backgroudTextSubTitle: 'El primer Campus Culinario del Perú, con capacidad para 4,000 estudiantes; además, contamos con las instalaciones más modernas del país, con una arquitectura y diseño pensado exclusivamente para la educación culinaria.'
+    }
+}
