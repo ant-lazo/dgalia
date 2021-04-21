4fcde75e5fc86ae45656df3d3d6e2b83369dea15
Jonatan Padilla <igjdpadillac@tutanota.com>
2021-04-21 15:13:13 -0500

    cambio de ruta en produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
     production: false,
-   // apiUrl: 'https://api.raymisa.suit.pe/api/v1/'
     apiUrl: 'http://localhost:48935/api/v1/'
-//     apiUrl: 'https://api.dgallia.suit.pe/api/v1/'
 };
