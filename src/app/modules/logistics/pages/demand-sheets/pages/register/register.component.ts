30d994e0c361c931164383ffe7c35cebaf918351
Jonatan Padilla <jdpadill@bancolombia.com.co>
2021-05-29 14:02:39 -0500

    se estructura compoennte para registro de hoja de demanda

--- src/app/modules/logistics/pages/demand-sheets/pages/register/register.component.ts
+++ src/app/modules/logistics/pages/demand-sheets/pages/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MeasuredUnitService } from 'app/modules/measured-units/services/measured-unit.service';
+import { CookingSchedule } from 'app/modules/programation/models/cooking-schedule.model';
+import { CookingScheduleService } from 'app/modules/programation/services/cooking-schedule.service';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +13,33 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
+  public request: Observable<any>;
+  public cookingSchedule: CookingSchedule;
+
   public cookingScheduleId: string;
 
   constructor(
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
+    private _cookingSchedule: CookingScheduleService,
+    private _measureUnits: MeasuredUnitService
   ) { }
 
   ngOnInit(): void {
-     this.cookingScheduleId=  this._activatedRoute.snapshot.params.cookingScheduleId;
+    this.cookingScheduleId = this._activatedRoute.snapshot.params.cookingScheduleId;
+    if (this.cookingScheduleId != 'none') this.setData();
   }
+
+  public setData(): void {
+    this.request = combineLatest([
+      this._cookingSchedule.getResume(Number(this.cookingScheduleId)),
+      this._measureUnits.getGetList(),
+      this._cookingSchedule.getById(this.cookingScheduleId)
+    ]).pipe(map((result: any) => {
+      this.cookingSchedule = result[2];
+      return result;
+    }));
+  }
+
+
 
 }
