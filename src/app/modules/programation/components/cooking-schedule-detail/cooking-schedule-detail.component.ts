52042d04d982280403b355d7cd917696b13e6b25
Jonatan Padilla <jdpadill@bancolombia.com.co>
2021-05-29 14:03:43 -0500

    modulo de detalle de programación

---
+++ src/app/modules/programation/components/cooking-schedule-detail/cooking-schedule-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CookingSchedule } from '../../models/cooking-schedule.model';
+
+@Component({
+  selector: 'app-cooking-schedule-detail',
+  templateUrl: './cooking-schedule-detail.component.html',
+  styleUrls: ['./cooking-schedule-detail.component.scss']
+})
+export class CookingScheduleDetailComponent implements OnInit {
+
+  @Input() cookingSchedule: CookingSchedule;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
