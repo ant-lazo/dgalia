f7f8a85c3de6cfac221ee3876969ba8bae09fe29
Jonatan Padilla <jdpadill@bancolombia.com.co>
2021-05-29 14:03:24 -0500

    se completa mapper de categoria

--- src/app/modules/product-category/services/mappers/product-category.mapper.ts
+++ src/app/modules/product-category/services/mappers/product-category.mapper.ts
@@ -2,8 +2,13 @@ import { Mapper } from "app/core/models/mapper.interface";
 import { ProductCategory } from "../../models/product-category.interface";
 
 export class ProductCategoryMapper implements Mapper<ProductCategory> {
-    fromJson(json: any): ProductCategory {
-        throw new Error("Method not implemented.");
+    fromJson (json: any): ProductCategory  {
+       return {
+           code: json.code,
+           enabled: true,
+           id: json.id,
+           name: json.name
+       };
     }
     toJson(doc: ProductCategory) {
         throw new Error("Method not implemented.");
