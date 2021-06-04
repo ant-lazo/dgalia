06ffb060ecf86cbeb221de5f43287d7a6dfeaa44
Jonatan Padilla <jdpadill@bancolombia.com.co>
2021-06-04 15:03:18 -0500

    info_module: some modifitcations to make a reusable component

--- src/app/shared/info/info.component.ts
+++ src/app/shared/info/info.component.ts
@@ -1,5 +1,5 @@
 import { Location } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-info',
@@ -11,12 +11,25 @@ export class InfoComponent implements OnInit {
   @Input() title: string;
   @Input() message: string;
   @Input() showButton: boolean;
+  @Input() buttonIcon: string;
+  @Input() buttonText: boolean;
+  @Input() returnBack: boolean;
+
+  @Output() butttonAction: EventEmitter<boolean> = new EventEmitter();
 
   constructor(
     public location: Location
   ) { }
 
   ngOnInit(): void {
+  }
+
+  public listenButtonClick(): void {
+    if (this.returnBack) {
+      this.location.back();
+    } else {
+      this.butttonAction.emit(true);
+    }
   }
 
 }
