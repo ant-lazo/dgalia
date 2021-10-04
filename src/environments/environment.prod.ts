fd4c56e9b25667300aae8d37e828c8195b3966a1
alvaro.burga <alvaro.burga@suit.pe>
2021-10-04 16:57:55 +0000

    Updated environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.dgallia.suit.pe/api/v1/'
-    //apiUrl: 'https://api.logistics.dgallia.edu.pe/api/v1/'
+    //apiUrl: 'https://api.dgallia.suit.pe/api/v1/'
+    apiUrl: 'https://api.logistics.dgallia.edu.pe/api/v1/'
 };
