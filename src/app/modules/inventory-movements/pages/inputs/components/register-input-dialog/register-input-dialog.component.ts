8bc826be69b0f0490c9ad4836ab0c247e6ccb539
victoriami <victoria9016@gmail.com>
2021-11-04 09:43:53 -0500

    better presentation

--- src/app/modules/inventory-movements/pages/inputs/components/register-input-dialog/register-input-dialog.component.ts
+++ src/app/modules/inventory-movements/pages/inputs/components/register-input-dialog/register-input-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { JsonResp } from 'app/core/interfaces/json-resp.interface';
-import { HeadquartesService } from 'app/modules/headquarter/services/headquartes.service';
+// import { HeadquartesService } from 'app/modules/headquarter/services/headquartes.service';
 import { Product } from 'app/modules/product/models/product.model';
 import { ProductService } from 'app/modules/product/services/product.service';
 import { AppNotificationsService } from 'app/shared/Services/app-notifications.service';
@@ -22,11 +22,28 @@ export class RegisterInputDialogComponent implements OnInit {
   constructor(
     private _dialogRef: MatDialogRef<RegisterInputDialogComponent>,
     private _toast: AppNotificationsService,
+    private _product: ProductService,
     private _inputs: InputsService,
+    private _builder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: { productCode: string }
-  ) { }
+  ) { 
+    this.setForm();
+  }
 
   ngOnInit(): void {
+    if (this.data?.productCode) this.setRequestData();
+    console.log("data: ", this.data);
+  }
+
+  private setRequestData(): void {
+    this.requestData = combineLatest([
+      this._product.findByCode(this.data?.productCode),
+    ]).pipe(
+      map((resp) => {
+        this.setForm(resp[0]);
+        return resp;
+      })
+    );
   }
 
   public onClose(): void {
@@ -34,10 +51,24 @@ export class RegisterInputDialogComponent implements OnInit {
   }
 
   public onRegister(): void {
-    // this._inputs.saveProduct(this.form.value).subscribe((resp: JsonResp) => {
-    //   this._toast.registerSuccess(null, resp.data);
-    //   this._dialogRef.close(true);
-    // });
+    this._inputs.saveProduct(this.form.value).subscribe((resp: JsonResp) => {
+      this._toast.registerSuccess(null, resp.data);
+      this._dialogRef.close(true);
+    });
+  }
+
+  private setForm(product?: Product): void {
+    this.form = this._builder.group({
+      headquarter_id: [null, Validators.required],
+      // igv: [product?.igv, Validators.required],
+      igv: [null, Validators.required],
+      product_code: [product?.code, Validators.required],
+      quantity: [null, Validators.required],
+      // unit_price: [product?.priceList, Validators.required],
+      unit_price: [null, Validators.required],
+      comment: [null, Validators.required],
+      output_type: [null, Validators.required],
+    });
   }
 
 }
